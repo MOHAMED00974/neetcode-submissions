fe5e7f7172915ed64f9ac70faf16878376affed6
Mohamed Tamer <142736549+MOHAMED00974@users.noreply.github.com>
2026-07-27 18:31:40 +0300

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        vis= {}
+        l, r= 0, 0
+        ans= 0
+        CurMax= 0
+
+        while r< len(s):
+            vis[s[r]]= vis.get(s[r], 0)+1
+            CurMax= max(CurMax, vis[s[r]])
+
+            if (r-l+1) -CurMax<= k:
+                r+= 1
+                continue
+            ans= max(ans, r-l)
+            
+            while (r-l+1) -CurMax> k:
+                vis[s[l]]-= 1
+                l+= 1
+            r+= 1
+        ans= max(ans, r-l)
+        return ans
